c9578abf1c525b2191088aa7c92f1463f33a0bd5
Aditya-mohapatra <adityamohapatra247@gmail.com>
2026-07-30 04:55:54 +0530

    Add: longest-substring-without-duplicates - submission-6

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-6.cpp
@@ -0,0 +1,33 @@
+class Solution {
+   public:
+    int lengthOfLongestSubstring(string s) {
+        int size = s.size();
+        int first = 0;
+        int last = 0;
+        int max_value = 0;
+
+        unordered_map<char, int> kmap;
+        if (size == 0) {
+            return 0;
+        };
+        if (size == 1) {
+            return 1;
+        };
+
+        while (last < size) {
+            if (kmap.find(s[last]) != kmap.end()) {
+                if (kmap[s[last]] >= first) {
+                    first = kmap[s[last]] + 1;
+                }
+                kmap[s[last]] = last;
+            } else {
+                kmap[s[last]] = last;
+            }
+            if (last - first + 1 > max_value) {
+                max_value = last - first + 1;
+            };
+            last++;
+        }
+        return max_value;
+    }
+};
